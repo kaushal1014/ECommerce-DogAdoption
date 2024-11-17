21b994aaea1260cd1a7543bdb78b32d2c72800ce
Kaushal Patil <kaushal.patil163@gmail.com>
2024-11-18 00:27:41 +0530

    Added product schema

--- server/models/Products.js
+++ server/models/Products.js
@@ -1,18 +1,24 @@
-// Import mongoose
 const mongoose = require('mongoose');
 
-// Define the Product schema
 const productSchema = new mongoose.Schema({
-  id: { type: Number, required: true },
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  price: { type: Number },
-  date: { type: Date, default: Date.now },
+  name: {
+    type: String,
+    required: true, // Set required as a boolean inside the field definition
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
 });
 
-// Create the Product model
 const Product = mongoose.model('Product', productSchema);
 
-// Export the model
 module.exports = Product;
