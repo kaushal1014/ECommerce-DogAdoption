b0b1c5264377161839a2c987bd065b174c79f00d
Kaushal Patil <kaushal.patil163@gmail.com>
2024-11-18 00:28:14 +0530

    Updated user schema

--- server/models/Users.js
+++ server/models/Users.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now },
 });
 
 // Create the Users model
